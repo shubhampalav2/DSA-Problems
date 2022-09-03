785d88d800790e085895d17fecc523f19a6e7404
Shubham Palav <87655660+shubhampalav2@users.noreply.github.com>
2022-09-03 16:21:37 +0530

    Update LongestSubarraySumDivisibleK.java

--- Arrays/LongestSubarraySumDivisibleK.java
+++ Arrays/LongestSubarraySumDivisibleK.java
@@ -26,3 +26,33 @@ class Solution{
 //Optimized Approach Time Complexity:O(n)
 //Space Complexity:O(n)
 //Using Hashing(HashMaps)
+
+class Solution{
+    int longSubarrWthSumDivByK(int a[], int n, int k)
+    {
+        // Complete the function
+    int sum=0;
+    int ans=0;
+    int rem=0;
+    HashMap<Integer,Integer> map=new HashMap<>();
+    //Inserting rem and index in the map
+    map.put(0,-1);
+    for(int i=0;i<n;i++){
+    sum+=a[i];
+    rem=sum%k;
+    if(rem<0){
+    rem+=k;
+    }
+    if(map.containsKey(rem)){
+    int idx=map.get(rem);
+    int result=i-idx;
+    ans=Math.max(ans,result);
+    }
+    else{
+    map.put(rem,i);
+    }
+    }
+     return ans;  
+    }
+ 
+}
