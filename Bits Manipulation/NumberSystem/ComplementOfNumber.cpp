87553e6f831ad685bfc83d074f1cd248786aba37
Shubham Palav <87655660+shubhampalav2@users.noreply.github.com>
2022-09-07 22:54:46 +0530

    Create ComplementOfNumber.cpp

---
+++ Bits Manipulation/NumberSystem/ComplementOfNumber.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number"<<endl;
+    cin>>n;
+    int m=n;
+    int mask=0;
+    while(m!=0){
+    //Taking Left Shift and making or with 1
+    mask=mask<<1|1;
+    m=m/2;
+    }
+    int ans=(~n)&mask;
+    cout<<"The one's complement of a number is "<<ans<<endl;
+    return 0;
+}
